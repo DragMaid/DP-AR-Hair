a54263dc7c759d3e85f40fe03da5a3d3abb0d569
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-11-19 16:26:59 +0800

    add live portrait pre-existing modules
    add contribution for live portrait
    
    add SPADE implementation
    
    add spade network
    
    add spade residual block
    
    add warping network
    
    add motion encoder
    
    add context decoder
    
    add appearance feature encoder
    
    add util functions
    
    update .gitignore
    
    fix readme file

---
+++ src/models/motion_extractor.py
@@ -0,0 +1,29 @@
+from torch import nn
+import torch
+
+from src.models.convnextv2 import convnextv2_tiny
+from src.models.util import filter_state_dict
+
+
+class MotionExtractor(nn.Module):
+    """
+    Encoder network for the motion features (E_m) in the paper
+    """
+
+    def __init__(self, **kwargs):
+        super(MotionExtractor, self).__init__()
+
+        # Harcoded model is convnextv2_base
+        self.detector = convnextv2_tiny(**kwargs)
+
+    def load_pretrained(self, init_path: str):
+        if init_path not in (None, ''):
+            state_dict = torch.load(
+                init_path, map_location=lambda storage, loc: storage)['model']
+            state_dict = filter_state_dict(state_dict, remove_name='head')
+            ret = self.detector.load_state_dict(state_dict, strict=False)
+            print(f'Load pretrained model from {init_path}, ret: {ret}')
+
+    def forward(self, x):
+        out = self.detector(x)
+        return out

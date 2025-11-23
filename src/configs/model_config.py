6f23f6f66cf0214f35a2e78633e7583cafc2ceb7
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-11-23 09:41:43 +0800

    change config names to be more intuitive

--- src/configs/model_config.py
+++ src/configs/model_config.py
@@ -72,4 +72,4 @@ def load_config(path: str | Path) -> ModelConfig:
 
 
 work_dir = os.path.dirname(os.path.realpath(__file__))
-config = load_config(Path(work_dir) / "configs.yaml")
+model_config = load_config(Path(work_dir) / "configs.yaml")

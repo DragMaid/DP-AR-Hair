0d1f21329c77ae0133e74a3c2e8aa1af0713ab61
FSXplay <skinoftheduck@gmail.com>
2025-11-26 13:26:57 +0800

    Create front end template

--- smart-style-salon-app/frontend/src/App.tsx
+++ smart-style-salon-app/frontend/src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
+import { Box, Heading, Container } from '@chakra-ui/react';
 import logo from './logo.svg';
+import Webcam from './components/Webcam';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Container maxW="1200px" mt="8">
+        <Heading as="h2" size="3xl">AR Hairstyle Preview</Heading>
+        <Webcam></Webcam>
+      </Container>
+    </Box>
   );
 }
-
-export default App;

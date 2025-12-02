aa1473de2d0e705304537fee7a9e97b206622868
FSXplay <skinoftheduck@gmail.com>
2025-12-02 19:40:07 +0800

    Upload image to server

--- smart-style-salon-app/backend/src/index.ts
+++ smart-style-salon-app/backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { createServer } from "http";
 import cors from "cors";
 import { Server } from "socket.io";
+import multer from "multer";
 
 const app = express();
 const server = createServer(app);
@@ -11,13 +12,37 @@ const io = new Server(server, {
         methods: ["GET", "POST"]
     }
 })
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'hairstyle_image');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const upload = multer({ storage: storage }).single('file');
 
+// Server init
 app.use(cors());
+app.use(express.static('hairstyle_image'));
 const PORT = process.env.PORT || 8080;
 app.get('/', (req: Request, res: Response) => {
     res.send(`Hello from ${PORT}`);
 });
 
+
+// POST: send image
+app.post('/image', (req: Request, res: Response) => {
+    console.log("Received image: ", req);
+
+    upload(req, res, err => {
+        if (err) res.sendStatus(500);
+        res.send(req.file);
+    });
+});
+
+
+// WebRTC Signal Server
 io.on("connection", (socket) => {
     socket.emit("me", socket.id);
 
--- smart-style-salon-app/frontend/src/components/ImageUploadBox.tsx
+++ smart-style-salon-app/frontend/src/components/ImageUploadBox.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "@/api/api";
 
 export default function ImageUploadBox() {
+    const [currentImage, setCurrentImage] = useState<any | null>(null);
+
     const uploadHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log("User uploaded: ", event.target.files);
+        const image = event.target.files?.[0];
+        if (!image) return;
+
+        const data = new FormData();
+        data.append("file", image);
+
+        api.post("/image", data)
+            .then(res => {
+                setCurrentImage(res.data);
+            });
     };
 
     return (
         <div>
-            <input type="file" name="file" onChange={uploadHandler}></input>
+            <div>
+                <input type="file" name="file" onChange={uploadHandler} />
+            </div>
+            <img src={`http://localhost:8080/image/${currentImage}`} />
         </div>
-    )
+    );
 }

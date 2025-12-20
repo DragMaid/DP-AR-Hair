91338797437bc79d3646a6ab9ec7760a2a3a4c7d
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-12-20 22:24:31 +0800

    optimize image mask to not use PIL

--- libs/face_parsing/models/utils.py
+++ libs/face_parsing/models/utils.py
@@ -1,6 +1,5 @@
-from typing import Union, List, Tuple
+from typing import Tuple
 import torch
-import numpy as np
 import torchvision.transforms as transforms
 from PIL import Image
 
@@ -27,54 +26,27 @@ def get_mask_by_idx(
     input_size=(512, 512),
     class_idx=17
 ):
-    """
-    Supports:
-    - PIL.Image (single)
-    - torch.Tensor CxHxW (single)
-    - torch.Tensor BxCxHxW (batch)
-    Returns binary mask: BxHxW (torch.uint8)
-    """
     if isinstance(images, Image.Image):
-        images = prepare_image(images, input_size)
+        images = prepare_image(images, input_size).unsqueeze(0)
+
     elif isinstance(images, torch.Tensor):
-        if images.ndim == 3:  # C,H,W
+        if images.ndim == 3:      # C,H,W
             images = images.unsqueeze(0)
         elif images.ndim != 4:
             raise ValueError("Tensor must be CHW or BCHW")
+
     else:
         raise TypeError("Input must be PIL.Image or Tensor")
 
-    images = images.to(device)  # move to GPU
-    B, _, _, _ = images.shape
+    images = images.to(device)
 
-    outputs = model(images)[0]          # outputs: B x num_classes x H x W
+    outputs = model(images)[0]            # B x num_classes x H x W
+    predicted = outputs.argmax(dim=1)     # B x H x W
 
-    predicted = outputs.argmax(dim=1)   # B x H x W
+    mask = (predicted == class_idx)
+    mask = mask.unsqueeze(1).to(torch.uint8)  # B x 1 x H x W
 
-    mask = (predicted == class_idx).to(torch.uint8)  # B x H x W
-
-    resized_masks = []
-    for i in range(B):
-        orig_w, orig_h = get_image_size(images[i])
-        pil_mask = Image.fromarray(mask[i].cpu().numpy() * 255)
-        resized = pil_mask.resize((orig_w, orig_h), Image.NEAREST)
-        resized_masks.append(torch.from_numpy(
-            np.array(resized) // 255))
-
-    # Stack → B x 1 x H_orig x W_orig
-    return torch.stack(resized_masks, dim=0).unsqueeze(1)
-
-
-def get_image_size(image):
-    if isinstance(image, Image.Image):
-        return image.size  # (W, H)
-
-    if isinstance(image, torch.Tensor):
-        # Tensor layout: C,H,W
-        _, h, w = image.shape
-        return (w, h)
-
-    raise TypeError("Unsupported image type")
+    return mask
 
 
 if __name__ == "__main__":

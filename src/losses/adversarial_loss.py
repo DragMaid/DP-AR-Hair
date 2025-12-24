bc3fd1d2c45fe6ea0ba3f5c9670334840d9e877a
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-12-25 07:35:11 +0800

    replace batch norm with spectral norm

--- src/losses/adversarial_loss.py
+++ src/losses/adversarial_loss.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.nn.init as init
+from torch.nn.utils.parametrizations import spectral_norm
 
 
 class PatchGANDiscriminator(nn.Module):
@@ -9,8 +10,8 @@ class PatchGANDiscriminator(nn.Module):
         pad_width = 1
 
         sequence = [
-            nn.Conv2d(n_in_channels, n_filters,
-                      kernel_size=kernel_size, stride=2, padding=pad_width),
+            spectral_norm(nn.Conv2d(n_in_channels, n_filters,
+                                    kernel_size=kernel_size, stride=2, padding=pad_width)),
             nn.LeakyReLU(0.2, False)
         ]
 
@@ -20,33 +21,32 @@ class PatchGANDiscriminator(nn.Module):
             filter_multiplier_prev = filter_multiplier
             filter_multiplier = min(2 ** n, 8)
             sequence += [
-                nn.Conv2d(n_filters * filter_multiplier_prev,
-                          n_filters * filter_multiplier,
-                          kernel_size=kernel_size,
-                          stride=2,
-                          padding=pad_width),
-                nn.BatchNorm2d(n_filters * filter_multiplier),
+                spectral_norm(nn.Conv2d(n_filters * filter_multiplier_prev,
+                                        n_filters * filter_multiplier,
+                                        kernel_size=kernel_size,
+                                        stride=2,
+                                        padding=pad_width)),
+                # nn.BatchNorm2d(n_filters * filter_multiplier),
                 nn.LeakyReLU(0.2, False)
             ]
 
         filter_multiplier_prev = filter_multiplier
         filter_multiplier = min(2 ** n_layers, 8)
         sequence += [
-            nn.Conv2d(n_filters * filter_multiplier_prev,
-                      n_filters * filter_multiplier,
-                      kernel_size=kernel_size,
-                      stride=1,
-                      padding=pad_width),
-            nn.BatchNorm2d(n_filters * filter_multiplier),
+            spectral_norm(nn.Conv2d(n_filters * filter_multiplier_prev,
+                                    n_filters * filter_multiplier,
+                                    kernel_size=kernel_size,
+                                    stride=1,
+                                    padding=pad_width)),
+            # nn.BatchNorm2d(n_filters * filter_multiplier),
             nn.LeakyReLU(0.2, False)
         ]
 
-        sequence += [nn.Conv2d(n_filters * filter_multiplier,
-                               1,
-                               kernel_size=kernel_size,
-                               stride=1,
-                               padding=pad_width)
-                     ]
+        sequence += [spectral_norm(nn.Conv2d(n_filters * filter_multiplier,
+                                             1,
+                                             kernel_size=kernel_size,
+                                             stride=1,
+                                             padding=pad_width))]
 
         self.model = nn.Sequential(*sequence)
 
@@ -54,11 +54,25 @@ class PatchGANDiscriminator(nn.Module):
         return self.model(in_image)
 
 
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        init.kaiming_normal_(m.weight.data, a=0.2,
-                             mode='fan_in', nonlinearity='leaky_relu')
-    elif classname.find('BatchNorm') != -1:
-        init.normal_(m.weight.data, 1.0, 0.02)
-        init.constant_(m.bias.data, 0)
+def weights_init(m: nn.Module):
+    # SpectralNorm wraps weights as `weight_orig`
+    if isinstance(m, nn.Conv2d):
+        if hasattr(m, "weight_orig"):
+            # Spectral-normalized Conv2d
+            init.kaiming_normal_(
+                m.weight_orig,
+                a=0.2,
+                mode="fan_in",
+                nonlinearity="leaky_relu",
+            )
+        else:
+            # Plain Conv2d (in case you ever keep one)
+            init.kaiming_normal_(
+                m.weight,
+                a=0.2,
+                mode="fan_in",
+                nonlinearity="leaky_relu",
+            )
+
+        if m.bias is not None:
+            init.constant_(m.bias, 0.0)
